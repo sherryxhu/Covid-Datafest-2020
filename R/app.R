b8a680a7202566cd7570bc808788c8e9ec9f50ce
Daniel Zhou <daniel.zhou2012@gmail.com>
2020-04-21 00:54:09 -0400

    R_Multiple_Specie

--- R/app.R
+++ R/app.R
@@ -5,6 +5,7 @@ df = read.csv(file = '../waqi-covid19-airqualitydata-2020.csv', encoding="UTF-8"
 df = df %>% mutate(Date = as.Date(Date))
 cities = sort(as.character(unique(df$City)))
 df_city = df %>% filter(City == cities[1])
+previous_city = cities[1]
 species = sort(as.character(unique(df_city$Specie)))
 ui <- dashboardPage(
   dashboardHeader(title = "COVID & Air Quality"),
@@ -31,8 +32,10 @@ ui <- dashboardPage(
                               choices= cities, #list("Beijing", "NYC")
                   ),
                   selectInput("specie",
-                              label = h3("specie"),
+                              label = h3("Specie"),
                               choices= species, #list("o3", "pm10")
+                              multiple = TRUE,
+                              selected = "pm25"
                   )
                 )
               )
@@ -49,24 +52,36 @@ ui <- dashboardPage(
 
 server <- function(input, output, session) {
   observe({
-    df_city = df %>% filter(City == input$city)
-    species = sort(as.character(unique(df_city$Specie)))
-  
-    updateSelectInput(session, "specie",
-                    choices = species,
-      )
+    print(input$specie)
+    print(typeof(input$Specie))
+    if (input$city != previous_city){
+      df_city = df %>% filter(City == input$city)
+      species = sort(as.character(unique(df_city$Specie)))
+      previous_city = input$city
+      updateSelectInput(session, "specie",
+                      choices = species,
+        )
+    }
   })
   output$plot1 <- renderPlot({
 
-    df_2 = df %>% 
-      filter((City == input$city) & (Specie == input$specie)) %>%
-      arrange(Date)
-    ggplot(data=df_2, aes(x=Date, y=median, group=1)) +
-      geom_line()+
-      geom_point()+
+
+    p = ggplot()
+    for (specie in input$specie){
+      df_2 = df %>% 
+        filter((City == input$city) & (Specie == input$specie)) %>%
+        arrange(Date)
+      p = p +
+        geom_line(data = df_2, aes(x=Date, y=median)) +
+        geom_point(data= df_2, aes(x=Date, y=median)) 
+    }
+    # ggplot(data=df_2, aes(x=Date, y=median, group=1)) +
+    #   geom_line()+
+    #   geom_point()+
+    p = p +
       ggtitle(paste(input$city, input$specie))+
       scale_x_date(date_breaks = "2 week")
-    
+    p
   })
 }
 
